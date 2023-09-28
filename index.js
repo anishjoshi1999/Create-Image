c1815d1c385989f47ff5aeddbeac9cd2b8162d5b
Anish Joshi <38525604+anishjoshi1999@users.noreply.github.com>
2023-09-28 09:51:11 +0545

    package.json updated

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const axios = require("axios");
 // Load environment variables
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // Connection to Database
 
