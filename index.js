821315c3c0f56923e943cfee583001e42e505c92
anishjoshi1999 <anishjoshi1999@gmail.com>
2023-09-28 20:14:56 +0545

    k

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const app = express();
+const cors = require("cors");
 const axios = require('axios'); // Import Axios
 const express = require('express');
-const app = express();
-const dotenv = require("dotenv");
-const cors = require("cors");
 
 // Load environment variables
 dotenv.config();
